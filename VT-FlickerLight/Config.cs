41e4fd83a9ad194529ca1c14439cfd612e5dcbb7
Antoniofo <89939643+Antoniofo@users.noreply.github.com>
2021-10-21 09:42:00 +0200

    Add files via upload

---
+++ VT-FlickerLight/Config.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Synapse.Config;
+
+namespace VT_FlickerLight
+{
+    public class Config : IConfigSection
+    {
+        [Description("How many times does the light flicker")]
+        public int NumberOfLightFlickingAtTheBegining { get; set; } = 10;
+
+        [Description("The first color of the light when the light start flicker. (R G B)")]
+        public int[] FirstColor { get; set; } = { 255, 0, 0 };
+
+        [Description("The second color of the light when the light start flicker. (R G B) WARNING: do not put the same number in first and second color")]
+        public int[] SecondColor { get; set; } = { 254, 200, 200 };
+
+        [Description("The numer of second between the two change color")]
+        public float TimeBetweenFlicker { get; set; } = 0.75f;
+    }
+}
---
+++ VT-FlickerLight/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("VT-FlickerLight")]
+[assembly: AssemblyDescription("Flicker the light at begining of the round")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VT-Dev")]
+[assembly: AssemblyProduct("VT-FlickerLight")]
+[assembly: AssemblyCopyright("Copyright © Antoniofo 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("ba9f5d7b-bf16-424c-a810-090365cc485a")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
